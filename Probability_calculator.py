bf26da4a2ee6fbaa918984e603cfc4f4c5ef61c8
Adam Richardson <highbeech.adam@ntlworld.com>
2017-02-08 23:02:41 +0000

    Add files via upload

--- Probability_calculator.py
+++ Probability_calculator.py
@@ -21,29 +21,29 @@ class Factorial:
 
 
 """Returns the total number of outcomes. For example rolling a fair 6 sided dice 50 times. Number of outcomes for one
-event would be 6 (event_outcomes), and the total number of events is 50 (total_events)."""
+event would be 6 (sing_event), and the total number of events is 50 (tot_events)."""
 
 
-def total_outcomes(event_outcomes, total_events):
-    return event_outcomes ** total_events
+def total_outcomes(sing_event, tot_events):
+    return sing_event ** tot_events
 
 
-"""This returns the number of wanted outcomes. For example flipping a fair coin 50 times. How many ways can you get
-25 heads? In this case the events would be 50 and the outcomes would be 25."""
+"""Returns the number of wanted outcomes. For example flipping a fair coin 50 times. How many ways can you get
+25 heads? In this case the events would be 50 (tot_events) and the outcomes would be 25 (wanted_outcomes)."""
 
 
-def choose(events, outcomes):
-    if events < outcomes:
+def choose(tot_events, wanted_outcomes):
+    if tot_events < wanted_outcomes:
         raise ValueError("The # of events should be bigger than the # of outcomes. Code will run anyway assuming you've"
                          "got the numbers the wrong way round")
-    diff = abs(events - outcomes)
-    return Factorial(events).factorial() / (Factorial(outcomes).factorial() * Factorial(diff).factorial())
+    diff = abs(tot_events - wanted_outcomes)
+    return Factorial(tot_events).factorial() / (Factorial(wanted_outcomes).factorial() * Factorial(diff).factorial())
 
 
 """Returns the probability of a certain event happening. For example flipping a fair coin 50 times. The probability of
 getting exactly 25 heads then the number of events is 50 (tot_events), the number of wanted outcomes is 25
-(tot_outcomes), and the number of outcomes for a single event is 2, heads or tails (sing_events)."""
+(wanted_outcomes), and the number of outcomes for a single event is 2, heads or tails (sing_events)."""
 
 
-def probability_of_event(tot_events, tot_outcomes, sing_event):
-    return choose(tot_events, tot_outcomes) / total_outcomes(sing_event, tot_events)
+def probability_of_event(tot_events, wanted_outcomes, sing_event):
+    return choose(tot_events, wanted_outcomes) / total_outcomes(sing_event, tot_events)
